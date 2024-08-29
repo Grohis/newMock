e276a71ac721383c5bc98e337eb80ff2c9c9b4a5
Grohis <166329543+Grohis@users.noreply.github.com>
2024-08-29 22:13:07 +0300

    first commit

---
+++ Smok/src/main/java/com/example/newMosk/Model/RequestDTO.java
@@ -0,0 +1,25 @@
+package com.example.newMosk.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class RequestDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String openDate;
+    private String closeDate;
+
+}
+
+
+//        "rqUID": "58dgtf565j8547f64ke7",
+//        "clientId": "3050000000000000000",
+//        "account": "30500000000000000001",
+//        "openDate": "2020-01-01",
+//        "closeDate": "2025-01-01"
